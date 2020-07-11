319af11b819130c1248527799ce9a4a257d834f5
Kevin Abrar Khansa <kevariable@gmail.com>
2020-07-11 18:47:44 +0700

    Register Integrated Github, Google, Facebook

--- config/services.php
+++ config/services.php
@@ -40,5 +40,11 @@ return [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
         'redirect' => env('GITHUB_CLIENT_REDIRECT')
+    ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_CLIENT_REDIRECT')
     ]
 ];
